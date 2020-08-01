15ba48d9f0111d09667b86ebf8fee22ab7e4978f
PJC-1 <jumpzine@gmail.com>
2020-07-31 23:27:01 -0700

    initial get request on document ready

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,29 @@
 $(document).on("ready", function(){
-  console.log("sanity check");
+
+  $.ajax({
+      method: "GET",
+      url: "http://api.giphy.com/v1/gifs/search?",
+      data: $("form").serialize(),
+      success: onSuccess,
+      error: onError
+  });
+
+  $(".btn").click(function(e){
+    console.log("button clicked");
+    e.preventDefault();
+    var q = $("form").serialize();
+    console.log("form data: ", q);
+  });
+
+  function onSuccess(json) {
+    console.log(json);
+  }
+
+  function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status " + status);
+    console.dir(xhr);
+  }
+
 });
